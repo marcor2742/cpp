67798b6262cd8ac2545b5348dd59a4bd5d24508f
mruggier <145976814+marcor2742@users.noreply.github.com>
2024-05-01 23:01:59 +0200

    Update HumanB.cpp

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -2,18 +2,18 @@
 
 HumanB::HumanB(std::string str, Weapon* obj) : name(str), weapon(obj)
 {
-    std::cout << "HumanB è Americano" << std::endl;
+    std::cout << this->name << " è Americano" << std::endl;
 }
 
 HumanB::HumanB(std::string str) : name(str)
 {
-    std::cout << "HumanB non è Americano" << std::endl;
+    std::cout << this->name << " non è Americano" << std::endl;
     this->weapon = NULL;
 }
 
 HumanB::~HumanB()
 {
-    std::cout << "HumanB è morto di vecchiaia" << std::endl;
+    std::cout << this->name << " è morto di vecchiaia" << std::endl;
 }
 
 void    HumanB::attack()
