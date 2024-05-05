bceaf6069c26ae28df9a1e17eed3738c843c3019
marcor2742 <marcoruggiero.mail@gmail.com>
2024-05-05 02:57:38 +0200

    done ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,47 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	std::cout << "------------vivo------------" << std::endl;
+}
+
+Harl::~Harl()
+{
+	std::cout << "------------morto-----------" << std::endl;
+}
+
+void Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!";
+}
+
+void Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+}
+
+void Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.";
+}
+
+void Harl::error( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.";
+}
+
+void Harl::complain( std::string level )
+{
+	std::string levelArray[4] = {"debug", "info", "warning", "error"};
+	void (Harl::*funcArray[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = 0;
+	while (level != levelArray[i] && i < 4)
+		i++;
+	if (i < 4)
+	{
+		(this->*funcArray[i])();
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "ma cah scrivi nel main" << std::endl;
+}
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main()
+{
+	Harl obj;
+
+	obj.complain("debug");
+	obj.complain("info");
+	obj.complain("warning");
+	obj.complain("error");
+}
