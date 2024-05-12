0be1374c27b1919a35332234851e4451bc4e58f9
marcor2742 <marcoruggiero.mail@gmail.com>
2024-05-13 00:06:09 +0200

    ex00 fatto

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -2,16 +2,18 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
+#include <string>
 
 class ClapTrap
 {
 private:
-	std::string name;
+	std::string Name;
 	int HitPoints;
 	int EnergyPoints;
 	int AttackDamage;
 public:
 	ClapTrap();
+	ClapTrap(std::string str);
 	ClapTrap(const ClapTrap &obj);
 	~ClapTrap();
 	ClapTrap &operator=(const ClapTrap &obj);
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+	ClapTrap gino("gino");
+	gino.attack("carlo");
+	gino.takeDamage(3);
+	gino.beRepaired(1);
+	gino.beRepaired(0);
+	gino.beRepaired(7);
+	gino.beRepaired(1);
+	gino.takeDamage(0);
+	gino.beRepaired(0);
+	gino.attack("carlo");
+	gino.takeDamage(10);
+	gino.takeDamage(1);
+	gino.beRepaired(1234);
+	gino.attack("carlo");
+	gino.attack("carlo");
+	gino.attack("carlo");
 	
 }
