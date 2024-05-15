90a64274f43fa9c2408c35cb369bf354739cb6a6
marcor2742 <marcoruggiero.mail@gmail.com>
2024-05-15 16:48:30 +0200

    [done][100%]

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,13 +6,12 @@
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 16:38:19 by mruggier          #+#    #+#             */
-/*   Updated: 2024/05/15 16:38:20 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/05/15 16:46:53 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
-#include "FragTrap.hpp"
 
 int main()
 {
@@ -30,13 +29,6 @@ int main()
 		luca.attack("gino");
 
 	std::cout << luca.getHitPoints() << std::endl;
-
-	std::cout << GREEN;
-	FragTrap carl("carl");
-	std::cout << END;
-	//for (int i = 0; i < 101; i++)
-		carl.attack("gino");
-	std::cout << carl.getHitPoints() << std::endl;
 	
 	std::cout << std::endl;
 
@@ -45,9 +37,6 @@ int main()
 	luca.beRepaired(234);
 	luca.takeDamage(32);
 	luca.guardGate();
-	carl.beRepaired(45);
-	carl.takeDamage(45);
-	carl.highFivesGuys();
 
 	std::cout << std::endl;
 	
