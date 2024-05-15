204b70b327e07b024afa25bcdb463a13100e4d46
marcor2742 <marcoruggiero.mail@gmail.com>
2024-05-15 09:57:14 +0200

    cpp di lmicheli

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -1,53 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marco <marco@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 12:42:32 by lmicheli          #+#    #+#             */
+/*   Updated: 2024/05/15 02:27:00 by marco            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap() : ScavTrap() {};
-
-DiamondTrap::DiamondTrap(std::string str) : ClapTrap(str + "_clap_trap"), ScavTrap(str + "_scav_trap"), FragTrap(str + "_frag_trap")
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name)
 {
-	std::cout << "DiamondTrap " << this->_name << " constructed" << std::endl;
-	std::cout << ScavTrap::HitPoints << std::endl;
-	std::cout << ScavTrap::EnergyPoints << std::endl;
-	std::cout << ScavTrap::AttackDamage << std::endl;
-	std::cout << ScavTrap::MaxHitPoints << std::endl;
-	std::cout << FragTrap::HitPoints << std::endl;
-	std::cout << FragTrap::EnergyPoints << std::endl;
-	std::cout << FragTrap::AttackDamage << std::endl;
-	std::cout << FragTrap::MaxHitPoints << std::endl;
-	this->_name = str;
-	this->ClapTrap::Name = _name + "_clap_name";
-	this->HitPoints = FragTrap::HitPoints;
-    this->AttackDamage = FragTrap::AttackDamage;
-    this->MaxHitPoints = ScavTrap::MaxHitPoints;
-    this->EnergyPoints = ScavTrap::EnergyPoints;
-	std::cout << "DiamondTrap " << this->_name << std::endl;
-	std::cout << this->HitPoints << std::endl;
-	std::cout << this->EnergyPoints << std::endl;
-	std::cout << this->AttackDamage << std::endl;
-	std::cout << this->MaxHitPoints << std::endl;
+	std::cout << "Yes! i'm \033[97mDiamondTrap\033[0m " << name << std::endl;
+	this->Name = name;
+	this->ClapTrap::Name = name + "_clap_name";
+
+	FragTrap::HitPoints = 100;
+	ScavTrap::EnergyPoints = 50;
+	ScavTrap::AttackDamage = 30;
+
+}
+/*DiamondTrap::DiamondTrap(DiamondTrap const & rhs) : ClapTrap(rhs), ScavTrap(rhs), FragTrap(rhs)
+{
+	std::cout << "Copy constructor for \033[97mDiamondTrap\033[0m called" << std::endl;
+	*this = rhs;
+}*/
+
+DiamondTrap & DiamondTrap::operator=(DiamondTrap const & rhs)
+{
+	std::cout << "Assignation operator for \033[97mDiamondTrap\033[0m called" << std::endl;
+	this->Name = rhs.Name;
+	this->HitPoints = rhs.HitPoints;
+	this->EnergyPoints = rhs.EnergyPoints;
+	this->AttackDamage = rhs.AttackDamage;
+	return *this;
 }
 
 DiamondTrap::~DiamondTrap()
 {
-	std::cout << "DiamondTrap " << this->_name << " destructed" << std::endl;
+	std::cout << "\033[97mDiamondTrap\033[0m " << this->Name << " is dead" << std::endl;
 }
 
 void DiamondTrap::whoAmI()
 {
-	std::cout << "DiamondTrap " << this->_name << " and Claptrap " << ClapTrap::Name << std::endl;
+	std::cout << "I'm " << this->Name << " and my ClapTrap name is " << this->ClapTrap::Name << std::endl;
 }
-
-void DiamondTrap::pEnergyPoint()
-{
-    std::cout << "ScavTrap::EnergyPoint = " << ScavTrap::EnergyPoints << std::endl;
-}
-
-//DiamondTrap::DiamondTrap(DiamondTrap const &rhs) : ClapTrap(rhs), ScavTrap(rhs), FragTrap(rhs) {}
-
-/*DiamondTrap &DiamondTrap::operator=(DiamondTrap const &rhs)
-{
-	if (this != &rhs)
-	{
-		ClapTrap::operator=(rhs);
-	}
-	return *this;
-}*/
--- cpp03/ex03/DiamondTrap.hpp
+++ cpp03/ex03/DiamondTrap.hpp
@@ -4,21 +4,16 @@
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
 
-class DiamondTrap : public virtual ScavTrap, public virtual FragTrap
+class DiamondTrap : public ScavTrap, public FragTrap
 {
 private:
 	std::string _name;
-	int HitPoints;
-	int EnergyPoints;
-	int AttackDamage;
-	int MaxHitPoints;
 	DiamondTrap();
 public:
 	DiamondTrap(std::string str);
 	~DiamondTrap();
 	DiamondTrap(DiamondTrap const &rhs);
 	DiamondTrap &operator=(DiamondTrap const &rhs);
-	void pEnergyPoint();
 
 	using ScavTrap::attack;
 	void whoAmI();
