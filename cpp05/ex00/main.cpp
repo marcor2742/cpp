05ff2d6c4177ff53060a6ed1c03062320cfc8280
marcor2742 <marcoruggiero.mail@gmail.com>
2024-06-17 18:03:55 +0200

    ex00

---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,50 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat mary("mary", 146);
+
+	try
+	{
+		Bureaucrat john("john", 0);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << "\n" <<std::endl;
+	}
+
+	Bureaucrat luca("luca", 10);
+	std::cout << luca;
+	for (int i = 0; true; ++i)
+	{
+		try
+		{
+			luca.incrementGrade();
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << "\n" << std::endl;
+			break;
+		}
+		std::cout << i << " ";
+	}
+	Bureaucrat reno(mary);
+	std::cout << reno;
+	for (int i = 0; true; ++i)
+	{
+		try
+		{
+			reno.decrementGrade();
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << "\n" <<std::endl;
+			break;
+		}
+		std::cout << i << " ";
+	}
+
+	Bureaucrat lisa;
+	lisa = luca;
+	std::cout << lisa << std::endl;
+}
