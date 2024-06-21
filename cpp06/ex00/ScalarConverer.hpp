ca61c1700f84d34a6cc0fcc2b960c1332fca4943
marcor2742 <marcoruggiero.mail@gmail.com>
2024-06-21 18:40:13 +0200

    ciao

--- cpp06/ex00/ScalarConverer.hpp
+++ cpp06/ex00/ScalarConverer.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 class ScalarConverter
 {
@@ -20,7 +21,7 @@ public:
 		public:
 			const char *what() const throw()
 			{
-				return "char not displayable";
+				return "not displayable";
 			}
 	};
 
@@ -33,43 +34,120 @@ public:
 			}
 	};
 
+	class OutOfBounds : public std::exception
+	{
+		public:
+			const char *what() const throw()
+			{
+				return "out of bounds";
+			}
+	};
+
+	static std::string checkCharString(std::string &str)
+	{
+		if (str.size() == 1 && ((str[0] >= '!' && str[0] <= '/') || (str[0] >= ':' && str[0] <= '~')))
+		{
+			std::cout << str << " is a char" << std::endl;
+			return "char";
+		}
+		for (std::string::size_type i = 0; i < str.size(); ++i)
+		{
+			char c = str[i];
+			if (!((c >= '0' && c <= '9') || c == 'f' || c == '.' || c == '-' || c == '+'))
+				return "";
+		}
+		if (str.find(".") == std::string::npos && str.find("f") == std::string::npos)
+		{
+			std::cout << str << " is a int" << std::endl;
+			return "int";
+		}
+		else if (str.find("f") == std::string::npos)
+		{
+			std::cout << str << " is a double" << std::endl;
+			return "double";
+		}
+		else
+		{
+			std::cout << str << " is a float" << std::endl;
+			return "float";
+		}
+		return "";
+	}
+
 	static void convert(std::string &literal)
 	{
 		int c;
         int i;
         float f;
         double d;
+		std::string desinenza;
 
-        std::stringstream ss(literal);
+		std::string str = checkCharString(literal);
+		if (str == "") //42.6 non fa * //nan +inf -inf
+		{
+			std::cout << "bad input" << std::endl;
+			return;
+		}
 
+		std::stringstream ss(literal);
+
+		if (str == "char")
+		{
+        	std::cout << "char: " << "'" << literal << "'" << std::endl;
+			c = static_cast<int>(literal[0]);
+			std::stringstream converter;
+        	converter << c;
+        	literal = converter.str();
+		}
+		else
+		{
+			try
+			{
+        		ss >> c;
+				if (ss.fail())
+					throw Impossible();
+				if (c <= 32 || c >= 126)
+					throw NotDisplayable();
+        		std::cout << "char: " << "'" << static_cast<char>(c) << "'" << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << "char: " << e.what() << '\n';
+			}
+        	ss.clear();
+		}
+		
 		try
 		{
-        	ss >> c;
-			if (ss.fail() || c <= 32 || c >= 126)
-				throw Impossible();
-			else
-        		std::cout << "char: " << "'" << static_cast<char>(c) << "'" << std::endl;
+			ss.str(literal);
+			ss >> i;
+			if (((i == 2147483647 && literal != "2147483647") || (i == -2147483648 && literal != "-2147483648")))
+				throw OutOfBounds();
+			std::cout << "int: " << i << std::endl;
+
 		}
 		catch(const std::exception& e)
 		{
-			std::cerr << e.what() << '\n';
+			std::cerr << "int: " << e.what() << '\n';
 		}
+		ss.clear();
+		
+
+		ss.str(literal);
+		ss >> f;
+		desinenza = "f";
+		if ((literal.find(".") == std::string::npos || literal.find(".0")) && literal.size() <= 6)
+			desinenza = ".0f";
+		std::cout << "float: " << f << desinenza << std::endl;	
 		
 
         ss.clear();
         ss.str(literal);
-        ss >> i;
-        std::cout << "int: " << i << std::endl;
-
-        ss.clear();
-        ss.str(literal);
-        ss >> f;
-        std::cout << "float: " << f << "f" << std::endl;
-
-        ss.clear();
-        ss.str(literal);
         ss >> d;
-        std::cout << "double: " << d << std::endl;
+		desinenza = "";
+		if (literal.find(".") == std::string::npos && literal.size() <= 6)
+			desinenza = ".0";
+        std::cout << "double: " << d << desinenza << std::endl;
 	}
 };
 
