f1908c5fca46d9972f43b851e79ef7065f9a0392
marcor2742 <marcoruggiero.mail@gmail.com>
2024-06-26 16:52:01 +0200

    fine ex00

--- cpp06/ex00/ScalarConverer.hpp
+++ cpp06/ex00/ScalarConverer.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <limits>
 
 class ScalarConverter
 {
@@ -12,143 +13,53 @@ private:
 	ScalarConverter() {}
 	ScalarConverter(const ScalarConverter& other) { *this = other; }
 	ScalarConverter& operator=(const ScalarConverter& other) { (void)other; return *this; }
+	static std::string checkCharString(std::string &str);
+    static bool checkPseudoLiteral(std::string &str);
+    static void convertChar(std::string &str, int &c, std::stringstream &ss, std::string &literal);
+    static void convertInt(int &i, std::stringstream &ss, std::string &literal, bool *isinf);
+    static void convertFloat(float &f, std::stringstream &ss, std::string &literal, bool *isinf);
+    static void convertDouble(double &d, std::stringstream &ss, std::string &literal, bool *isinf);
 
 public:
 	~ScalarConverter() {}
+	static void convert(std::string &literal);
 
 	class NotDisplayable : public std::exception
 	{
 		public:
-			const char *what() const throw()
-			{
-				return "not displayable";
-			}
+			const char *what() const throw() { return "not displayable"; }
 	};
 
 	class Impossible : public std::exception
 	{
 		public:
-			const char *what() const throw()
-			{
-				return "impossible";
-			}
+			const char *what() const throw() { return "impossible"; }
 	};
 
 	class OutOfBounds : public std::exception
 	{
 		public:
-			const char *what() const throw()
-			{
-				return "out of bounds";
-			}
+			const char *what() const throw() { return "out of bounds"; }
 	};
 
-	static std::string checkCharString(std::string &str)
+	class PlusInfinity : public std::exception
 	{
-		if (str.size() == 1 && ((str[0] >= '!' && str[0] <= '/') || (str[0] >= ':' && str[0] <= '~')))
-		{
-			std::cout << str << " is a char" << std::endl;
-			return "char";
-		}
-		for (std::string::size_type i = 0; i < str.size(); ++i)
-		{
-			char c = str[i];
-			if (!((c >= '0' && c <= '9') || c == 'f' || c == '.' || c == '-' || c == '+'))
-				return "";
-		}
-		if (str.find(".") == std::string::npos && str.find("f") == std::string::npos)
-		{
-			std::cout << str << " is a int" << std::endl;
-			return "int";
-		}
-		else if (str.find("f") == std::string::npos)
-		{
-			std::cout << str << " is a double" << std::endl;
-			return "double";
-		}
-		else
-		{
-			std::cout << str << " is a float" << std::endl;
-			return "float";
-		}
-		return "";
-	}
+		public:
+			const char *what() const throw() { return "+inf"; }
+	};
 
-	static void convert(std::string &literal)
+	class MinusInfinity : public std::exception
 	{
-		int c;
-        int i;
-        float f;
-        double d;
-		std::string desinenza;
+		public:
+			const char *what() const throw() { return "-inf"; }
+	};
 
-		std::string str = checkCharString(literal);
-		if (str == "") //42.6 non fa * //nan +inf -inf
-		{
-			std::cout << "bad input" << std::endl;
-			return;
-		}
+	class NotANumber : public std::exception
+	{
+		public:
+			const char *what() const throw() { return "nan"; }
+	};
 
-		std::stringstream ss(literal);
-
-		if (str == "char")
-		{
-        	std::cout << "char: " << "'" << literal << "'" << std::endl;
-			c = static_cast<int>(literal[0]);
-			std::stringstream converter;
-        	converter << c;
-        	literal = converter.str();
-		}
-		else
-		{
-			try
-			{
-        		ss >> c;
-				if (ss.fail())
-					throw Impossible();
-				if (c <= 32 || c >= 126)
-					throw NotDisplayable();
-        		std::cout << "char: " << "'" << static_cast<char>(c) << "'" << std::endl;
-			}
-			catch(const std::exception& e)
-			{
-				std::cerr << "char: " << e.what() << '\n';
-			}
-        	ss.clear();
-		}
-		
-		try
-		{
-			ss.str(literal);
-			ss >> i;
-			if (((i == 2147483647 && literal != "2147483647") || (i == -2147483648 && literal != "-2147483648")))
-				throw OutOfBounds();
-			std::cout << "int: " << i << std::endl;
-
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << "int: " << e.what() << '\n';
-		}
-		ss.clear();
-		
-
-		ss.str(literal);
-		ss >> f;
-		desinenza = "f";
-		if ((literal.find(".") == std::string::npos || literal.find(".0")) && literal.size() <= 6)
-			desinenza = ".0f";
-		std::cout << "float: " << f << desinenza << std::endl;	
-		
-
-        ss.clear();
-        ss.str(literal);
-        ss >> d;
-		desinenza = "";
-		if (literal.find(".") == std::string::npos && literal.size() <= 6)
-			desinenza = ".0";
-        std::cout << "double: " << d << desinenza << std::endl;
-	}
 };
 
 #endif
---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,204 @@
+#include "ScalarConverer.hpp"
+
+	std::string ScalarConverter::checkCharString(std::string &str)
+	{
+		double d;
+
+		if (str.size() == 1 && ((str[0] >= '!' && str[0] <= '/') || (str[0] >= ':' && str[0] <= '~')))
+		{
+			std::cout << str << " is a char" << std::endl;
+			return "char";
+		}
+		for (std::string::size_type i = 0; i < str.size(); ++i)
+		{
+			char c = str[i];
+			if (!((c >= '0' && c <= '9') || c == 'f' || c == '.' || c == '-' || c == '+'))
+				return "";
+		}
+
+		std::stringstream ss(str);
+		ss.str(str);
+		ss >> d;
+		ss.clear();
+
+		if (str.find(".") == std::string::npos && str.find("f") == std::string::npos && str.size() <= 12 && d <= 2147483647 && d >= -2147483648)
+		{
+			std::cout << str << " is a int" << std::endl;
+			return "int";
+		}
+		else if (str.find("f") == std::string::npos)
+		{
+			if (d == std::numeric_limits<double>::max() || d == -std::numeric_limits<double>::max() || d <= std::numeric_limits<double>::min())
+				return "infinity";
+			std::cout << str << " is a double" << std::endl;
+			return "double";
+		}
+		else
+		{
+			std::cout << str << " is a float" << std::endl;
+			return "float";
+		}
+		return "";
+	}
+
+	bool ScalarConverter::checkPseudoLiteral(std::string &str)
+	{
+		std::string pseudoarray[8] = {"nan", "nanf", "+inf", "+inff", "inf", "inff", "-inf", "-inff"};
+		for (int i = 0; i < 8; i++)
+		{
+			if (str == pseudoarray[i])
+			{
+				if (i == 0 || i == 1)
+					std::cout << "char: impossible" << "\n"
+						<< "int: impossible" << "\n"
+						<< "float: nanf" << "\n"
+						<< "double: nan" << std::endl;
+				if (i >= 2)
+				{
+					int tmp = 0;
+					int tmpf = 0; 
+					if (i % 2 != 0)
+						tmp = -1;
+					else
+						tmpf = + 1;
+					std::cout << "char: impossible" << "\n"
+						<< "int: impossibile" << "\n"
+						<< "float: " << pseudoarray[i + tmpf] << "\n"
+						<< "double: " << pseudoarray[i + tmp] << std::endl;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void ScalarConverter::convertChar(std::string &str, int &c, std::stringstream &ss, std::string &literal)
+	{
+		if (str == "char")
+		{
+        	std::cout << "char: " << "'" << literal << "'" << std::endl;
+			c = static_cast<int>(literal[0]);
+			std::stringstream converter;
+        	converter << c;
+        	literal = converter.str();
+		}
+		else
+		{
+			try
+			{
+        		ss >> c;
+				if (ss.fail() || c < 0 || c > 127)
+					throw Impossible();
+				if (c <= 32 || c >= 126)
+					throw NotDisplayable();
+        		std::cout << "char: " << "'" << static_cast<char>(c) << "'" << std::endl;
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << "char: " << e.what() << '\n';
+			}
+        	ss.clear();
+		}
+	}
+
+	void ScalarConverter::convertInt(int &i, std::stringstream &ss, std::string &literal, bool *isinf)
+	{
+		try
+		{
+			ss.str(literal);
+			ss >> i;
+			if (isinf[0] == true || isinf[1] == true)
+				throw Impossible();
+			if (((i == 2147483647 && literal != "2147483647") || (i == -2147483648 && literal != "-2147483648")))
+				throw OutOfBounds();
+			std::cout << "int: " << i << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "int: " << e.what() << '\n';
+		}
+		ss.clear();
+	}
+
+	void ScalarConverter::convertFloat(float &f, std::stringstream &ss, std::string &literal, bool *isinf)
+	{
+		std::string desinenza;
+
+		try
+		{
+			ss.str(literal);
+			ss >> f;
+			desinenza = "f";
+			if (isinf[0] == true)
+				throw PlusInfinity();
+			if (isinf[1] == true)
+				throw MinusInfinity();
+			if ((f == std::numeric_limits<float>::max() && literal.length() > 38) || (f == -std::numeric_limits<float>::max() && literal.length() > 38))
+				{
+					desinenza = "";
+					throw OutOfBounds();
+				}
+			if (literal.size() <= 6 && (literal.find(".") == std::string::npos || literal.find(".0") != std::string::npos))
+				desinenza = ".0f";
+			std::cout << "float: " << f << desinenza << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "float: " << e.what() << desinenza << '\n';
+		}
+	}
+
+	void ScalarConverter::convertDouble(double &d, std::stringstream &ss, std::string &literal, bool *isinf)
+	{
+		std::string desinenza;
+
+		try
+        {
+		ss.clear();
+        ss.str(literal);
+        ss >> d;
+		desinenza = "";
+		if (literal.size() <= 6 && (literal.find(".") == std::string::npos || literal.find(".0") != std::string::npos))
+			desinenza = ".0";
+		if (isinf[0] == true)
+			throw PlusInfinity();
+		if (isinf[1] == true)
+			throw MinusInfinity();
+        std::cout << "double: " << d << desinenza << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "double: " << e.what() << '\n';
+		}
+	}
+
+	void ScalarConverter::convert(std::string &literal)
+	{
+		int c;
+        int i;
+        float f;
+        double d;
+		bool isinf[2] = {false, false};
+
+		if (checkPseudoLiteral(literal) == true)
+			return;
+		std::string str = checkCharString(literal);
+		if (str == "")
+		{
+			std::cout << "bad input" << std::endl;
+			return;
+		}
+
+		std::stringstream ss(literal);
+		convertChar(str, c, ss, literal);
+		ss.str(literal);
+		ss >> d;
+		if (d == std::numeric_limits<double>::max() && literal.length() > 308)
+			isinf[0] = true;
+		if (d == -std::numeric_limits<double>::max() && literal.length() > 308)
+			isinf[1] = true;
+		ss.clear();
+		convertInt(i, ss, literal, isinf);
+		convertFloat(f, ss, literal, isinf);
+		convertDouble(d, ss, literal, isinf);
+	}
