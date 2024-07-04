1343582eec6ffa409549239bc640a4af736a10e3
marcor2742 <marcoruggiero.mail@gmail.com>
2024-07-05 01:25:59 +0200

    inizio ex01

---
+++ cpp09/ex01/main.cpp
@@ -0,0 +1,8 @@
+/*
+usa stack
+se il numero di argomenti è minore di 3 errore
+se i primi due argomenti non sono numeri allora errore
+operatori -1 = operandi, sennò errore
+se c'è qualcosa di diverso da 1 2 3 4 5 6 7 8 9 0 + - * / " " allora errore
+se i numeri sono < 0 o > 9 errore ((((((((lo 0 è compreso?))))))))
+*/
